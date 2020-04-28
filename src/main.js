a8728444bc27be34cc0603b470d3e15f9f661eb1
unknown <leslieezhu@qq.com>
2020-04-28 22:48:15 +0800

    202004282249

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-    var app = new Vue({
-        el: '#app',
+    var app = new Vue({  //读作，创建vue对象，并传入对象
+        el: '#app',  //意义：让vue接管页面上的哪个元素，即vue绑定页面上哪个元素，术语：实例，挂载点，模板 直接的关系
         data: {
           message: 'Hello Vue!'
         }
